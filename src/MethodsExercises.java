c0b7654e5c343fd257789361bbe1eb0d5d059af5
taylor lindholm <taylor.lindholm01@gmail.com>
2021-04-27 14:42:07 -0500

    feat: afternoon push.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -24,4 +24,16 @@ public class MethodsExercises {
     public static long modulus(int num1, int num2){
         return num1 % num2;
     }
+    public static int getInteger(int min,int max){
+        Scanner scn = new Scanner(System.in);
+        int answer = scn.nextInt();
+        if (answer <= 1 || answer >= 10){
+            getInteger();
+        }
+        else if (answer >= 1 && answer <= 10){
+          System.out.println("Thank you");
+        }
+
+        if
+    }
 }
